10b84f515003e6b0695bac5f0c0238fd40cd123b
Asbjørn Skødt <asbjoern.skoedt@gmail.com>
2019-05-27 13:01:57 +0200

    Add files via upload

---
+++ undertoner-indstillinger/undertoner-indstillinger.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name: Undertoner indstillinger
+ * Description: Plugin til håndtering af indstillinger til Undertoners design.
+ * Version: 1.0
+ * Author: Asbjørn Skødt
+ * Author URI: https://youtu.be/boIKuEl6l0Y
+ **/
+ 
+require_once dirname( __FILE__ ) . '/undertoner-plader/undertoner-plader.php';
+require_once dirname( __FILE__ ) . '/undertoner-reklamebanner/undertoner-reklamebanner.php';
+
+?>
