93580e2e4a096c591c68171f25c8a6995d2f892f
Asbjørn Skødt <asbjoern.skoedt@gmail.com>
2019-10-06 21:22:30 +0200

    Update undertoner-reklamebanner.php

--- undertoner-indstillinger/undertoner-reklamebanner/undertoner-reklamebanner.php
+++ undertoner-indstillinger/undertoner-reklamebanner/undertoner-reklamebanner.php
@@ -5,9 +5,9 @@ function vis_undertoner_reklamebanner() {
     if ( get_option( 'undertoner_reklamebanner_boolean' ) === '1' ) {
         echo '<div class="reklamebanner_container">
 				<div class="reklamebanner">
-                <a href="' . get_option( 'undertoner_reklamebanner_hjemmeside' ). '" target="_blank">
-                    <img src="' . get_option( 'undertoner_reklamebanner_reklamebillede' ). '" />
-                </a>
+					<a href="' . get_option( 'undertoner_reklamebanner_hjemmeside' ). '" target="_blank">
+						<img src="' . get_option( 'undertoner_reklamebanner_reklamebillede' ). '" width="' . get_option( 'undertoner_reklamebanner_bredde' ). 'px" height="' . get_option( 'undertoner_reklamebanner_hoejde' ). 'px" />
+					</a>
 				</div>
 			</div>';
     }
@@ -15,8 +15,8 @@ function vis_undertoner_reklamebanner() {
 
 // Forbinder plugin CSS-fil til Undertoners design
 function undertoner_reklamebanner_css() {
-    $plugin_url = plugin_dir_url( __DIR__ );
-    wp_enqueue_style( 'undertoner_reklamebanner_style', $plugin_url . '/undertoner-reklamebanner/undertoner-reklamebanner.css' );
+    $plugin_url = plugin_dir_url( __FILE__ );
+    wp_enqueue_style( 'undertoner_reklamebanner_style', $plugin_url . '/undertoner-reklamebanner.css' );
 }
 add_action( 'wp_enqueue_scripts', 'undertoner_reklamebanner_css' );
 
